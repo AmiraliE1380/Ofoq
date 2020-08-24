da853864acdaa4286802fed4b01569b8417836a1
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-08-24 18:01:18 +0430

    Created admin API

--- articles/models.py
+++ articles/models.py
@@ -3,10 +3,14 @@ from django.db import models
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('تاریخ انتشار:')
+    pub_date = models.DateField('تاریخ انتشار:')
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
-    reply_id = models.IntegerField()
+    reply_id = models.IntegerField(default=-1)
     username = models.CharField(max_length=100)
-    content = models.CharField()
+    content = models.CharField(max_length=500)
+    pub_date = models.DateTimeField()
