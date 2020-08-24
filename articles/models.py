47cd23de58dd3ec6486e4fcd572cc67e06e0f938
Amirali <ebrahimzadeh.amirali@gmail.com>
2020-08-24 11:55:45 +0430

    Partly implemented models.py in articles

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('تاریخ انتشار:')
+
+
+class Comment(models.Model):
+    reply_id = models.IntegerField()
+    username = models.CharField(max_length=100)
+    content = models.CharField()
